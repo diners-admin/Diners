a2ad0de6f160471a4b2b6f278bc00c79e24ebb9c
diners-admin <45186395+diners-admin@users.noreply.github.com>
2018-11-25 05:43:48 +0100

    Add files via upload

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Diners-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "DinersURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
